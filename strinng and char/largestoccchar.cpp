25bb6c8b55532ee1175856043705a1d13b545ad6
raajshekhar19 <raajshekharbgp@gmail.com>
2024-06-26 21:22:59 +0530

    largest occuring char in a string

--- strinng and char/largestoccchar.cpp
+++ strinng and char/largestoccchar.cpp
@@ -1,18 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+char maxelement(string s){
+    int arr[26]={0};
+    for(int i =0;i<s.length();i++){
+        char ch = s[i];
+        //lowercase
+        if(ch>='a' && ch<='s'){
+            int n =ch - 'a' ;
+            arr[n]++;
+        }
+        else{
+            int n =ch-'A' ;
+            arr[n]++;
+        }
+    }
+
+    int maxi = 0;
+    int ans = 0;
+    for(int i = 0;i<26;i++){
+        if(arr[i]>maxi){
+            maxi= arr[i];
+            ans = i;
+        }
+    }
+
+    return (char)(ans + 'a');
+}
+
 int main(){
     string str = "testsample";
     int length = str.length();
-    map<char,int> mpp;
-    for(int i =0;i<length;i++){
-        mpp[str[i]]++;
-    }
-    int largest = -1;
-    for(auto it : mpp){
-        if(it.second>largest){
-            largest=it.second;
-        }
-    }
+    cout<<maxelement(str);
     
 }
