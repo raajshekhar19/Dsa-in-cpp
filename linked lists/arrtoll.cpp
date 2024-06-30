9a317e080b5b6a5bf3c01a1d996985b056881ea5
raajshekhar19 <raajshekharbgp@gmail.com>
2024-06-30 19:03:45 +0530

    about single  ll

--- linked lists/arrtoll.cpp
+++ linked lists/arrtoll.cpp
@@ -18,8 +18,21 @@ class Node {
     }
 };
 
+Node * convertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for(int i = 1 ;i<arr.size();i++){
+        Node * temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+
 int main(){
     vector<int> arr = {2,5,8,7};
-    Node* y = new Node(arr[0] );
-    cout<<y->data;
+    Node* head = convertArr2LL(arr);
+    cout<<head->data;
 }
---
+++ linked lists/lenghtofALL.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public :
+    int data;
+    Node* next;
+
+    public :
+    Node(int data1, Node* next1){ 
+        data = data1;
+        next = next1;
+    }
+    public :
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node * convertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for(int i = 1 ;i<arr.size();i++){
+        Node * temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+int lengthofall(Node* head){
+    int counter = 0;
+    Node* temp = head;
+    while(temp){
+        temp=temp->next;
+        counter++;
+    }
+    return counter;
+}
+
+
+int main(){
+    vector<int> arr = {2,5,8,7};
+    Node* head = convertArr2LL(arr);
+    cout<<lengthofall(head);
+}
