fd54042a464745c6c752b54a64dbc1e2c5492a64
raajshekhar19 <raajshekharbgp@gmail.com>
2024-07-02 21:24:18 +0530

    delete the head of the node

---
+++ linked lists/insertinll.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public :
+    int data;
+    Node* next;
+
+    public :
+    Node(int data, Node* next){ 
+        this->data = data;
+        this->next = next;
+    }
+    public :
+    Node(int data){
+        this->data = data;
+        this->next = nullptr;
+    }
+
+    
+
+
+};
+
+int main(){
+    
+}
