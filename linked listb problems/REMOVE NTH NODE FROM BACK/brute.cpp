1920adc7e81998d6f62e8ccb1eb6f391b4740847
raajshekhar19 <raajshekharbgp@gmail.com>
2024-07-20 21:09:46 +0530

    remove the nth node from the end

---
+++ linked listb problems/REMOVE NTH NODE FROM BACK/brute.cpp
@@ -0,0 +1,34 @@
+/* ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+        int length = 0;
+        ListNode * temp = head;
+        while(temp){
+            length++;
+            temp = temp->next;
+        }
+        int k = length - n;
+        if(length == n){
+            ListNode * temp = head;
+            head = head->next;
+            temp->next = NULL;
+            delete temp;
+            return head; 
+        }
+
+        else{
+            ListNode * temp = head;
+            int counter = 0;
+            while(temp){
+                counter++;
+                if(counter==k){
+                    break;
+                }
+                temp= temp->next;
+            }
+            ListNode * dummy = temp->next;
+                    temp->next = temp->next->next;
+                    dummy->next = NULL;
+                    delete dummy;
+                    return head;
+        }
+    } */
